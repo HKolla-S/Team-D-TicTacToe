6aaf7bf4b0bce4469c2024255ce3e44f4601ede6
HKolla-S <68656947+HKolla-S@users.noreply.github.com>
2020-07-25 14:10:11 -0700

    Main module

--- main.py
+++ main.py
@@ -2,3 +2,61 @@
 #This is the file to run
 
 from tictactoe import TicTacToe
+
+print('Welcome to Tic Tac Toe!')
+
+board = []
+letter = ''
+move = ''
+bo = ''
+le = ''
+
+self = TicTacToe(board, letter, move, bo, le)
+
+while True:
+    # Reset the board
+    theBoard = [' '] * 10
+    player1Letter, player2Letter = TicTacToe.inputPlayerLetter(self)
+    turn = TicTacToe.whoGoesFirst(self)
+    print('The ' + turn + ' will go first.')
+    gameIsAlive = True
+
+    while gameIsAlive:
+        if turn == 'player 1':
+            # Player 1's turn.
+            TicTacToe.drawBoard(self, theBoard)
+            move = TicTacToe.getPlayer1Move(self, theBoard)
+            TicTacToe.makeMove(self, theBoard, player1Letter, move)
+            if TicTacToe.isWinner(self, theBoard, player1Letter):
+                TicTacToe.drawBoard(self, theBoard)
+                print('Player 1 has won!')
+                gameIsAlive = False
+                break
+            else:
+                if TicTacToe.isBoardFull(self, theBoard):
+                    TicTacToe.drawBoard(self, theBoard)
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 'player 2'
+        else:
+            # Player 2's turn.
+            TicTacToe.drawBoard(self, theBoard)
+            move = TicTacToe.getPlayer2Move(self, theBoard)
+            TicTacToe.makeMove(self, theBoard, player2Letter, move)
+            if TicTacToe.isWinner(self, theBoard, player2Letter):
+                TicTacToe.drawBoard(self, theBoard)
+                print('Player 2 has won!')
+                gameIsAlive = False
+                break
+            else:
+                if TicTacToe.isBoardFull(self, theBoard):
+                    TicTacToe.drawBoard(self, theBoard)
+                    print('The game is a tie!')
+                    break
+                else:
+                    turn = 'player 1'
+
+    nNext = TicTacToe.playAgain(self)
+    if not nNext.lower().startswith('y'):
+        break
--- tictactoe.py
+++ tictactoe.py
@@ -2,14 +2,12 @@
 import random
 
 class TicTacToe:
-    def __init__(self, board, letter, cMove, bo, le, movesList, compLetter):
+    def __init__(self, board, letter, move, bo, le):
         self.board = board
         self.letter = letter
-        self.move = cMove
+        self.move = move
         self.bo = bo
         self.le = le
-        self.movesLit = movesList
-        self.computerLetter = compLetter
 
     def drawBoard(self, board):
         # This function prints out the board that it was passed.
@@ -44,14 +42,15 @@ class TicTacToe:
     def whoGoesFirst(self):
         # Randomly choose the player who goes first.
         if random.randint(0, 1) == 0:
-            return 'computer'
+            return 'player 2'
         else:
-            return 'player'
+            return 'player 1'
 
     def playAgain(self):
         # This function returns True if the player wants to play again, otherwise it returns False.
         print('Do you want to play again? (yes or no)')
-        return input().lower().startswith('y')
+        nNext = input()
+        return nNext
 
     def makeMove(self, board, letter, move):
         board[move] = letter
@@ -81,62 +80,21 @@ class TicTacToe:
         # Return true if the passed move is free on the passed board.
         return board[move] == ' '
 
-    def getPlayerMove(self, board):
-        # Let the player type in his move.
+    def getPlayer1Move(self, board):
+        # Let player 1 type in his move.
         move = ' '
         while move not in '1 2 3 4 5 6 7 8 9'.split() or not self.isSpaceFree(board, int(move)):
-            print('What is your next move? (1-9)')
+            print('Player 1, What is your move? (1-9)')
             move = input()
         return int(move)
 
-    def chooseRandomMoveFromList(self, board, movesList):
-        # Returns a valid move from the passed list on the passed board.
-        # Returns None if there is no valid move.
-        possibleMoves = []
-        for i in movesList:
-            if self.isSpaceFree(board, i):
-                possibleMoves.append(i)
-
-        if len(possibleMoves) != 0:
-            return random.choice(possibleMoves)
-        else:
-            return None
-
-    def getComputerMove(self, board, computerLetter):
-        # Given a board and the computer's letter, determine where to move and return that move.
-        if computerLetter == 'X':
-            playerLetter = 'O'
-        else:
-            playerLetter = 'X'
-
-        # Here is our algorithm for our Tic Tac Toe AI:
-        # First, check if we can win in the next move
-        for i in range(1, 10):
-            copy = self.getBoardCopy(board)
-            if self.isSpaceFree(copy, i):
-                self.makeMove(copy, computerLetter, i)
-                if self.isWinner(copy, computerLetter):
-                    return i
-
-        # Check if the player could win on his next move, and block them.
-        for i in range(1, 10):
-            copy = self.getBoardCopy(board)
-            if self.isSpaceFree(copy, i):
-                self.makeMove(copy, playerLetter, i)
-                if self.isWinner(copy, playerLetter):
-                    return i
-
-        # Try to take one of the corners, if they are free.
-        move = self.chooseRandomMoveFromList(board, [1, 3, 7, 9])
-        if move != None:
-            return move
-
-        # Try to take the center, if it is free.
-        if self.isSpaceFree(board, 5):
-            return 5
-
-        # Move on one of the sides.
-        return self.chooseRandomMoveFromList(board, [2, 4, 6, 8])
+    def getPlayer2Move(self, board):
+        # Let player 2 type in his move.
+        move = ' '
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not self.isSpaceFree(board, int(move)):
+            print('Player 2, What is your move? (1-9)')
+            move = input()
+        return int(move)
 
     def isBoardFull(self, board):
         # Return True if every space on the board has been taken. Otherwise return False.
